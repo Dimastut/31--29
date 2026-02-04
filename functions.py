633ebbffdf264b292252a2a1b1cbd35046ea240c
Dimastut <dimka.vlasov.452@gmail.com>
2026-02-04 12:19:09 +0300

    file fail

--- functions.py
+++ functions.py
@@ -11,3 +11,10 @@ def divide(a, b):
  if b == 0:
     raise ValueError("Деление на ноль невозможно")
  return a / b
+
+def is_valid_password(password: str) -> bool:
+ return len(password) >= 8
+def test_password_too_short(password:str) -> bool:
+  return len(password)>= 5
+def test_password_boundary(password: str)-> bool:
+  return len(password)>= 8
--- test_functions.py
+++ test_functions.py
@@ -1,11 +1,13 @@
 import pytest
-from functions import add, get_user_full_name
+from functions import add, get_user_full_name, divide, is_valid_password,  test_password_too_short, test_password_boundary
+# from pytest import markers 
+@pytest.mark.math
 def test_add():
  assert add(2, 3) == 5
  assert add(2, 2) == 4
  assert add(10, -5) == 5
  
-
+@pytest.mark.user_profile
 def test_get_user_full_name():
  user = {
  "first_name": "Dima",
@@ -13,7 +15,9 @@ def test_get_user_full_name():
  "email": "dimka.vlasov.452@gmail.com"
  }
  assert get_user_full_name(user) == "Dima Vlasov"
-6
+
+
+@pytest.mark.user_profile
 def test_user_has_email():
  user = {
  "first_name": "Dima",
@@ -22,17 +26,18 @@ def test_user_has_email():
  }
  assert "email" in user
 
-# ШАГ 1: Создаем фикстуру
+
 @pytest.fixture
 def sample_user_data():
- """Фикстура, которая возвращает словарь с данными пользователя."""
- return {
+  return {
  "first_name": "Dima",
  "last_name": "Vlasov",
  "email": "dimka.vlasov.452@gmail.com"
  }
+@pytest.mark.user_profile
 def test_get_user_full_name_with_fixture(sample_user_data: dict[str, str]):
  assert get_user_full_name(sample_user_data) == "Dima Vlasov"
+@pytest.mark.user_profile
 def test_user_has_email_with_fixture(sample_user_data: dict[str, str]):
  assert "email" in sample_user_data
 
@@ -53,3 +58,28 @@ from functions import divide
 def test_divide_by_zero_raises_error():
  with pytest.raises(ValueError):
   (divide(10, 0))
+
+@pytest.mark.skip(reason="Эта функция еще в разработке")
+def test_new_feature():
+# Код теста для новой, еще не готовой функции
+ assert False
+
+@pytest.mark.xfail(reason="Известный точностью float, тикет #123")
+def test_float_precision_bug():
+  assert (0.1 + 0.2) == 0.3 
+
+def test_password_valid():
+ password = "1234567890"
+ result = is_valid_password(password)
+ assert result == True
+
+ def test_password_too_short():
+  password = "12345"
+  result =  is_valid_password(password)
+  assert result == False
+
+  def test_password_boundary ():
+   password = "12345678"
+   result =  is_valid_password(password)
+   assert result = True
+
